a76f780354a05a5f4ffd6e6cc14c5897bf4fd958
Nasimi Taghizade <tenasimi@gmail.com>
2020-01-27 16:35:38 +0400

    Tic tac toe Final2

--- win_check.py
+++ win_check.py
@@ -5,18 +5,18 @@ from IPython.display import clear_output
 def player_input():
     marker = ' '
     while not (marker == 'X' or marker == '0'):
-        marker = input('Dear Player: Do you want to be X or 0? ').upper()
+        marker = input('Player 1: Do you want to be X or 0? ').upper()
     if marker == 'X':
         return ('X', '0')
     else:
         return ('0', 'X')
-# 5
+# 2
 def choose_first():
     if random.randint(0, 1) == 0:
         return 'Player 2'
     else:
         return 'Player 1'
-
+#3
 def display_board(board):  #board is a list with indexes
     clear_output()
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
@@ -26,7 +26,6 @@ def display_board(board):  #board is a list with indexes
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
 
 board=["#","X","O","X","O","X","O","X","O","X"]
-# 2
 # 4
 def win_check(board, mark):
     return (
@@ -41,23 +40,23 @@ def win_check(board, mark):
             (board[7] == mark and board[5] == mark and board[3] == mark) or
             (board[9] == mark and board[5] == mark and board[1] == mark)
     )
-# 6
+# 5
 def space_check(board, position):
     return board[position] == ' '        #for itself going a boolean - reurn used
 
-# 7                                 proverka na nalicie pustogo mesta + #6 dlya proverki Nicyi
+# 6                                 proverka na nalicie pustogo mesta + #6 dlya proverki Nicyi
 def full_board_check(board):
     for i in range(1, 10):
         if space_check(board, i):
             return False
     return True
-# 8
+# 7
 def player_choice(board):
     position = 0
     while position not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or not space_check(board, position):
         position = int(input('Choose your next position: (1-9) '))
     return position
-# 3
+# 8
 def place_marker(board, marker, position):
     board[position] = marker
 # 9
